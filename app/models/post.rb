07736b2ffa996723c598ab8ef38349b5606ea65a
megan <megan.schertz@gmail.com>
2021-03-11 15:54:12 -0600

    commit tree b7124a40b4f2429b316b3c92dff4494b9a312424

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,5 @@
 class Post < ActiveRecord::Base
+    validates :title, presence: true
+    validates :content, presence: true, length: { minimum: 100}
+    validates :category, presence: true, :inclusion => { :in => ['Fiction', 'Non-Fiction'] }
 end
